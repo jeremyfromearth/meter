4312572d426594737f6970fdeaf86e06421ffe46
jeremyfromearth <jeremynealbrown@gmail.com>
2017-01-04 23:24:45 -0800

    Set up build configuration

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var Paths = {
+    Build: path.resolve(__dirname, 'build'),
+    App: path.resolve(__dirname, 'app'),
+}
+
+var config = {
+    entry: {
+        app: Paths.App + "/main.js"
+    },
+
+    output: {
+        path: Paths.Build,
+        filename: '[name].js'
+    },
+
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+
+    module: {
+        loaders: [{
+            test: /\.js/,
+            loader: 'babel-loader',
+            include: Paths.App,
+            exclude: /(node_modules)/,
+        }]
+    }, 
+    
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Meter'
+        })
+    ]
+}
+
+module.exports = config;

9e0f117bc55f4c3662fcbc7a22e6a89ca2daef0a
jeremyfromearth <jeremynealbrown@gmail.com>
2017-01-14 19:34:57 -0800

    Added feature to disable/enable auto scrolling in output panel when mouse enters and leaves

--- app/js/output-panel.js
+++ app/js/output-panel.js
@@ -1,3 +1,4 @@
+import * as d3 from 'd3'
 import {Widget} from 'phosphor/lib/ui/widget'
 
 class OutputPanel extends Widget {
@@ -6,17 +7,30 @@ class OutputPanel extends Widget {
         this.store = store;
         this.store.subscribe('output_panel', this.on_output_panel_update.bind(this));
 
+        this.auto_scroll = true;
         this.addClass('content');
-        this.node.innerHTML = `<div id='terminal' class='terminal'>\> Welcome to Meter</div>`
+        this.node.innerHTML = `<div id='terminal' class='terminal'>\> Welcome to Meter</div>`;
     }
-    
+
+    onAfterAttach(message) {
+        d3.select('#terminal')
+            .on('mouseenter', ()=> {
+                this.auto_scroll = false; 
+            })
+            .on('mouseleave', ()=> {
+                this.auto_scroll = true; 
+            });
+    }
+
     on_output_panel_update(data) {
-        var div = document.getElementById('terminal');
+        var div = this.node.getElementsByTagName('div')[0];
         if(data.output_panel.length) {
             var message = data.output_panel[data.output_panel.length-1];
             div.insertAdjacentHTML(
                 'beforeend', `<br/>${message}`)
-            div.scrollTop = div.scrollHeight;
+            if(this.auto_scroll) {
+                div.scrollTop = div.scrollHeight;
+            }
         }
     }
 }

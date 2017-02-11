a5af9432d170717abb40d0552cd2b43ef9ddec25
jeremyfromearth <jeremynealbrown@gmail.com>
2017-02-11 00:02:37 -0800

    Began working on search filter UI

--- client/app/js/file-browser.js
+++ client/app/js/file-browser.js
@@ -1,3 +1,4 @@
+import * as d3 from 'd3'
 import {Observable} from 'rxjs'
 import {Widget} from 'phosphor/lib/ui/widget'
 
@@ -9,10 +10,33 @@ class FileBrowser extends Widget {
 
         this.addClass('content');
         this.title.label = 'Files';
+        this.filters_visible = false;
         this.node.innerHTML = 
-            `<div>
+            `<div class='file-browser-container'>
                 <div class='file-browser-search-input-container'>
-                    <input id='search-input' type='text'></input>
+                    <div>
+                        <div class='file-browser-search-filters-toggle'>Search</div>
+                        <input id='search-input' type='text'></input>
+                    </div>
+                    <div id='filter-selection'>
+                        <div id='search-filters-toggle' class='file-browser-search-filters-toggle'>Details</div>
+                        <ul id='filter-list'>
+                            <li><input type='checkbox' checked='true'/>Artist</li>
+                            <li><input type='checkbox' checked='true'/>Album</li>
+                            <li><input type='checkbox' checked='true'/>Composer</li>
+                            <li><input type='checkbox' checked='true'/>Song Title</li>
+                            <li><input type='checkbox' checked='true'/>Genre</li>
+                            <ul>
+                                <li><input type='checkbox' checked='true'/>Alternative</li>
+                                <li><input type='checkbox' checked='true'/>Blues</li>
+                                <li><input type='checkbox' checked='true'/>Country</li>
+                                <li><input type='checkbox' checked='true'/>Hip Hop</li>
+                                <li><input type='checkbox' checked='true'/>Pop</li>
+                            </ul>
+                            <li><input type='checkbox' checked='true'/>Year</li>
+                            <li><input type='text' placeholder='date range (ex. 1954-1976)'/></li>
+                        </ul>
+                    </div>
                 </div>
                 <div>
                     <div id='midi-library-list'/>
@@ -21,7 +45,15 @@ class FileBrowser extends Widget {
     }
 
     onAfterAttach(message) {
-        /*
+        var details_toggle = document.getElementById('search-filters-toggle');
+        details_toggle.addEventListener('click', () => {
+            var filters_list = document.getElementById('filter-list'); 
+            console.log(filters_list);
+            this.filters_visible = !this.filters_visible;
+            d3.select(filters_list)
+                .style('display', this.filters_visible ? 'inherit' : 'none');
+        });
+
         var search_input = document.getElementById('search-input');
         var keyup = Observable.fromEvent(search_input, 'keyup')
             .debounceTime(250)
@@ -34,7 +66,6 @@ class FileBrowser extends Widget {
             console.log(text);
             return {}
         });
-        */
     }
 
     update_list(data) {

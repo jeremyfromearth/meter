9f4dc5ec6393a18ae67562acd176d2cd86bae1bf
jeremyfromearth <jeremynealbrown@gmail.com>
2017-02-13 23:30:41 -0800

    Started working on script for scraping usable data from midi files for storage in database

---
+++ tools/midi-files-to-db.py
@@ -0,0 +1,39 @@
+import os
+from mido import MidiFile, MetaMessage, Message, tempo2bpm
+
+for f in os.listdir('./'):
+    if f.endswith('.mid'):
+        print('----------------------')
+        print(f)
+        try:
+            m = MidiFile('./' + f)
+            duration = 0
+            bpms = {}
+            bpm_duration = 0
+            current_bpm = 120
+            for msg in m:
+                duration += msg.time 
+                bpm_duration += msg.time
+                if isinstance(msg, MetaMessage):
+                    if msg.type != 'lyrics' and msg.type != 'midi_port':
+                        if msg.type == 'set_tempo':
+                            if current_bpm in bpms:
+                                bpms[current_bpm] += bpm_duration
+                            else:
+                                bpms[current_bpm] = bpm_duration
+                            bpm_duration = 0
+                            current_bpm = round(tempo2bpm(msg.tempo))
+                else:
+                    if msg.type == 'program_change':
+                        pass
+            if current_bpm in bpms:
+                bpms[current_bpm] += bpm_duration
+            else:
+                print('bpm_duration', bpm_duration)
+                bpms[current_bpm] = bpm_duration
+
+            print(duration)
+            print(max(bpms, key=bpms.get))
+        except Exception as e:
+            print('Could not open file', f)
+            print(e)

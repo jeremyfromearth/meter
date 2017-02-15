cff1de205949c63f35a20c092933d745cf169d73
jeremyfromearth <jeremynealbrown@gmail.com>
2017-02-14 23:15:11 -0800

    Continued working on supporting some primitive form of keyword and categorical search

--- tools/midi-files-to-db.py
+++ tools/midi-files-to-db.py
@@ -1,6 +1,7 @@
 import os
 import re
 import sys
+from random import random
 from mido import MidiFile, MetaMessage, Message, tempo2bpm
 
 dirname = ''
@@ -10,21 +11,9 @@ except:
     print('Directory name should be first argument')
     sys.exit()
 
-stop_words = set()
-with open('stopwords.txt') as f:
-    stop_words = set([line.rstrip() for line in f])
-
-composers = set()
-with open('composers.txt') as f:
-    composers = set([line.rstrip().lower() for line in f])
-
-musicians = set()
-with open('musicians.txt') as f:
-    musicians = set([line.rstrip().lower() for line in f])
-
-soundtrack_composers = set()
-with open('soundtrack-composers.txt') as f:
-    soundtrack_composers = set([line.rstrip().lower() for line in f])
+def create_set_from_file(filename):
+    with open(filename) as f:
+        return set([line.rstrip().lower() for line in f])
 
 def clean_text(t, trim=0):
     result = t.lower()
@@ -36,22 +25,39 @@ def clean_text(t, trim=0):
 def create_ngrams(l, n):
     return zip(*[l[i:] for i in range(n)])
 
+# percentage of filess to analyze
+sample_size = 0.00001
+
+# load terms from files
+stop_words = create_set_from_file('stopwords.txt')
+composers = create_set_from_file('composers.txt')
+musicians = create_set_from_file('artists.txt') 
+soundtrack_composers = create_set_from_file('soundtrack-composers.txt')
+genres = create_set_from_file('genres.txt')
+songs = create_set_from_file('top-10k-songs.txt')
+videogames = create_set_from_file('video-game-titles.txt')
+instruments = create_set_from_file('instruments.txt')
+
+# iterate over all files in supplied directory
 for f in os.listdir(dirname):
     if f.endswith('.mid') or f.endswith('.MID'):
-        print('----------------------')
-        print(f)
-        try:
-            m = MidiFile(dirname +'/'+ f)
-            duration = 0
-            bpms = {}
-            bpm_duration = 0
-            current_bpm = 120
-            programs = set()
-            words = set(clean_text(f, 4))
-            for msg in m:
-                duration += msg.time 
-                bpm_duration += msg.time
-                if isinstance(msg, MetaMessage):
+        r = random()
+        if r < sample_size:
+            print('----------------------')
+            print(f)
+            try:
+                m = MidiFile(dirname +'/'+ f)
+                duration = 0
+                bpms = {}
+                bpm_duration = 0
+                current_bpm = 120
+                programs = set()
+                filename = clean_text(f, 4)
+                words = [] + (filename)
+                for msg in m:
+                    duration += msg.time 
+                    bpm_duration += msg.time
+
                     if msg.type == 'set_tempo':
                         if current_bpm in bpms:
                             bpms[current_bpm] += bpm_duration
@@ -59,32 +65,46 @@ for f in os.listdir(dirname):
                             bpms[current_bpm] = bpm_duration
                         bpm_duration = 0
                         current_bpm = round(tempo2bpm(msg.tempo))
+                    
+                    if msg.type == 'text':
+                        words += set(clean_text(msg.text))
+
+                    if msg.type == 'track_name':
+                        words += clean_text(msg.name)
+
                     if msg.type == 'program_change':
                         programs.add(msg.program)
-                    if msg.type == 'text':
-                        words |= set(clean_text(msg.text))
-                    '''
-                    if msg.type == 'track_name':
-                        words |= set(clean_text(msg.name))
-                    '''
-            if current_bpm in bpms:
-                bpms[current_bpm] += bpm_duration
-            else:
-                bpms[current_bpm] = bpm_duration
-            words = words.difference(stop_words)
-            print('BPM', max(bpms, key=bpms.get))
-            print('Programs', programs)
-            print('Duration', m.length)
-            for i in range(1, 4):
-                ngrams = create_ngrams(list(words), i)
-                for gram in ngrams:
-                    phrase = ' '.join(gram) 
-                    if phrase in musicians:
-                        print('Found Musician', phrase)
-                    if phrase in composers:
-                        print('Found Composer', phrase)
-                    if phrase in soundtrack_composers:
-                        print('Found Soundtrack Composer', phrase)
-        except Exception as e:
-            print('Could not open file', f)
-            print(e)
+
+                if current_bpm in bpms:
+                    bpms[current_bpm] += bpm_duration
+                else:
+                    bpms[current_bpm] = bpm_duration
+                for word in words:
+                    if word in stop_words:
+                        words.remove(word)
+                print('Filename:', filename)
+                print('BPM:', max(bpms, key=bpms.get))
+                print('Programs:', programs)
+                print('Duration:', m.length)
+                for i in range(1, 8):
+                    ngrams = create_ngrams(words, i)
+                    for gram in ngrams:
+                        phrase = ' '.join(gram) 
+                        if phrase in musicians:
+                            print('Musician:', phrase)
+                        if phrase in composers:
+                            print('Composer:', phrase)
+                        if phrase in soundtrack_composers:
+                            print('Soundtrack Composer:', phrase)
+                        if phrase in genres:
+                            print('Genre:', phrase)	
+                        if phrase in songs:
+                            print('Song:', phrase)
+                        if phrase in videogames:
+                            print('Video Game:', phrase)
+                        if phrase in instruments:
+                            print('Instrument:', phrase)
+				
+            except Exception as e:
+                print('Could not open file', f)
+                print(e)

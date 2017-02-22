eae7d462b2cd07dc79c46e5e00a59fc0f01309ec
jeremyfromearth <jeremynealbrown@gmail.com>
2017-02-21 23:59:58 -0800

    Cleaned up to tfidf script

--- tools/midi-to-tfidf.py
+++ tools/midi-to-tfidf.py
@@ -1,3 +1,4 @@
+import sys
 from math import log
 from random import random
 from pymongo import MongoClient
@@ -9,26 +10,30 @@ db = client.meter
 
 i = 0
 tfidf_data = {}
-sample_rate = 0.00001
-for f in db.midi_files.find():
+sample_rate = 1.0
+count = db.midi_files.count()
+cursor = db.midi_files.find()
+for f in cursor:
     n = random()
     if n < sample_rate:
+        i += 1
         tfidf_data[f['filename']] = f
+        print('\r', 'Processing {} of {} files'.format(i, count), end='')
 
 df = DataFrame.from_dict(tfidf_data, orient='index')
-print(df.head())
 tfidf = TermFreqInverseDocFreq()
 tfidf.create(df, 'keywords', False)
+tfidf.save('midi-tfidf')
 
-def get_expanded(x):
-    return log(1.1 * len(x.name)) * x
+# This part should live on the server
+#def get_expanded(x):
+    #return log(1.1 * len(x.name)) * x
 
-for k, v in tfidf_data.items():
-    print(k)
+#for k, v in tfidf_data.items():
+    #print(k)
     #print(tfidf_data[k]['filename'])
-    terms = tfidf.get_sorted_terms_for_document(k)
+    #terms = tfidf.get_sorted_terms_for_document(k)
     #print(terms)
-    terms_df = terms.to_frame()
-    terms_df['expanded'] = terms_df.apply(get_expanded, axis=1)
-    print(terms_df)
-#tfidf.save('midi-tfidf')
+    #terms_df = terms.to_frame()
+    #terms_df['expanded'] = terms_df.apply(get_expanded, axis=1)
+    #print(terms_df)

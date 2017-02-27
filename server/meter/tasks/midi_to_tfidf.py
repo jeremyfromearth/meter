9a5458905168f1c8ec8e342fe59103dd90f8a40c
jeremyfromearth <jeremynealbrown@gmail.com>
2017-02-26 22:05:55 -0800

    Added to script so that it now updates objects in the db with scores for tfidf and rank

--- server/meter/tasks/midi_to_tfidf.py
+++ server/meter/tasks/midi_to_tfidf.py
@@ -13,30 +13,30 @@ def run(dest, sample_rate = 1.0):
     tfidf_data = {}
     sample_rate = sample_rate
     count = db.midi_files.count()
-    cursor = db.midi_files.find()
-    for f in cursor:
-        n = random()
-        if n < sample_rate:
+    for f in db.midi_files.find({}, {'checksum' : 1, 'raw_text': 1, 'filename': 1}):
+        if random() < sample_rate:
             i += 1
-            tfidf_data[f['filename']] = f
-            print('\r', 'Processing {} of {} files'.format(i, count), end='')
+            if 'checksum' in f:
+                tfidf_data[f['checksum']] = f
+                print('\r', 'Processing {} of {} files'.format(i, count), end='')
+            else:
+                print('ERROR: Checksum not found for {}'.format(f))
 
     df = DataFrame.from_dict(tfidf_data, orient='index')
     tfidf = TermFreqInverseDocFreq()
     tfidf.create(df, 'raw_text', True)
-    tfidf.save(dest + '/midi-tfidf.pkl')
+    #tfidf.save(dest + '/midi-tfidf.pkl')
 
-    '''
-    # This part should live on the server
-    def get_expanded(x):
-        return log(1.1 * len(x.name)) * x
+    print('\nTFIDF Complete')
+    print('Starting rank operation')
 
-    for k, v in tfidf_data.items():
-        print(k)
-        print(tfidf_data[k]['filename'])
-        terms = tfidf.get_sorted_terms_for_document(k)
-        print(terms)
-        terms_df = terms.to_frame()
-        terms_df['expanded'] = terms_df.apply(get_expanded, axis=1)
-        print(terms_df)
-    '''
+    i = 1
+    checksums= list(tfidf.doc_id_to_index.keys())
+    for checksum in checksums:
+        print('\r', 'Processing {} of {} files'.format(i, count), end='')
+        i += 1
+        scores = {}
+        terms = tfidf.get_sorted_terms_for_document(checksum)
+        for x in terms.index:
+            scores[x] = {'tfidf': terms[x], 'rank': log(len(x)) * terms[x]}
+        db.midi_files.find_one_and_update({'checksum': checksum}, {'$set' : {'scores' : scores}}, {})
